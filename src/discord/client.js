cc6e76706293ba100fb52a0e01ee77aeaf48f381
xlegendario <dario.b1996@gmail.com>
2026-02-12 16:12:08 +0100

    Create client.js

---
+++ src/discord/client.js
@@ -0,0 +1,12 @@
+import { Client, GatewayIntentBits, Partials } from "discord.js";
+
+export function createDiscordClient() {
+  return new Client({
+    intents: [
+      GatewayIntentBits.Guilds,
+      GatewayIntentBits.GuildMessages,
+      GatewayIntentBits.MessageContent
+    ],
+    partials: [Partials.Channel, Partials.Message]
+  });
+}
