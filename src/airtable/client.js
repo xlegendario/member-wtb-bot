00e31241902437b87ad1f02fc4bd7b4159e25f5d
xlegendario <dario.b1996@gmail.com>
2026-08-22 17:27:13 +0200

    Delete src directory

--- src/airtable/client.js
+++
@@ -1,6 +0,0 @@
-import Airtable from "airtable";
-import { CONFIG, assertConfig } from "../config.js";
-
-assertConfig();
-
-export const base = new Airtable({ apiKey: CONFIG.airtableApiKey }).base(CONFIG.airtableBaseId);
